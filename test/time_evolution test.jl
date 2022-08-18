574e7af8c9b5e2011226bc137a2e6126b7d5c4b8
tripathydevjyoti <devjyoti.tripathy@students.iiserpune.ac.in>
2022-08-18 06:59:54 +0530

    Update time_evolution test.jl

--- test/time_evolution test.jl
+++ test/time_evolution test.jl
@@ -2,39 +2,36 @@ using Test
 using LinearAlgebra
 
 function time_evolution(
-    Eₙ::Array{T, 1},
+    Eₙ::Vector{Float64},
     #ψₙ::Array{T, 2},
-    ψₙ::Vector{AbstractArray{Int64}},
+    ψₙ::Vector{Vector{Int64}},
     ψ₀::Array{Complex{T}, 1},
     t::T
 ) where T <: Real
-    cₙ = dot.(conj.(ψ₀), ψₙ)
-    eₙ = exp.(-1im .* Eₙ .* t)
-    sum(eₙ .* cₙ .* ψₙ, dims=2)
+     
+    function inner_product(
+        ψ ::Vector{Int64}
+    ) 
+        δ = dot(Ψ₀,ψ)
+    end    
+
+    
+    cₙ = inner_product.(v)
+    eₙ = exp.(-1im .* e .*t)
+    sum(eₙ .* cₙ .* v)
 end
 
-e = [0.5,-0.5]
-
-
-v = [[1,0],[0,1]]
-    
-Ψ₀ = [1/sqrt(2), 1/sqrt(2)]
-Ψ₀ = complex(Ψ₀)
-
-
-
-
-time_evolution(e, v, Ψ₀, 1.0)
+ 
 
 @testset "Checking if time_eolution is working fine" begin
     e = [0.5,-0.5]
 
-
+    c =1/sqrt(2)
     v = [[1,0],[0,1]]
     
     Ψ₀ = [1/sqrt(2), 1/sqrt(2)]
     Ψ₀ = complex(Ψ₀)
 
-    time_evolution(e, v, Ψ₀, 1.0)
+    @test time_evolution(e, v, Ψ₀, 1.0) == [ c*exp(-0.5im), c*exp(+0.5im)]
     
 end
