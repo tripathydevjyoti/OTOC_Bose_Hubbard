ab91bd0801dba9791241d96d8a5d76568d139242
bartekGardas <bartek.gardas@gmail.com>
2022-08-20 12:30:55 +0200

    add benchmark

--- benchmark/bench.jl
+++ benchmark/bench.jl
@@ -1,6 +1,12 @@
 
+using OTOC_Bose_Hubbard
+
+function bench(M::Int, N::Int)
+    J, U = 1.0, 0.5
+    BoseHubbard(N, M, J, U, :OBC)
+end
+
 M = N = 13
-
-@time B = Basis(M, N)
-
+@time bench(M, N)
+@time bench(M, N)
 nothing
