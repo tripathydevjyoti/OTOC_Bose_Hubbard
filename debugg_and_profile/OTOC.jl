f7bc0bb736ec138259e5efc866eaa1984a2c4aa8
bartekGardas <bartek.gardas@gmail.com>
2022-08-22 14:34:39 +0200

    clean up

--- debugg_and_profile/OTOC.jl
+++ debugg_and_profile/OTOC.jl
@@ -7,6 +7,7 @@ using FlameGraphs
 function bench(N, M, K, graph)
     T = Float64
     B = NBasis([N, N-1, N-2], M)
+    println("Dim: ", B.dim)
     OTOC(
         BoseHubbard(B, T(4/10), zero(T), graph),
         1, 2,
@@ -16,12 +17,12 @@ function bench(N, M, K, graph)
     0
 end
 
-M = 8
-N = 8
+M = 10
+N = 10
 K = 10
 graph = star_digraph(M)
 
 bench(N, M, K, graph)
 @profile bench(N, M, K, graph)
 
-pprof(flamegraph(); webhost = "localhost", webport = 51416)
+pprof(flamegraph(); webhost = "localhost", webport = 51316)
