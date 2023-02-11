dcfb12042f09b14961d85f67827e1e56bb9ae449
zakaria <zakaria.mzaouali@gmail.com>
2023-02-11 21:53:56 +0100

    update

--- examples/QuantumWorkStatistics.jl
+++ examples/QuantumWorkStatistics.jl
@@ -1,37 +1,78 @@
 using OTOC_Bose_Hubbard
 using LinearAlgebra
 using Plots
+using LaTeXStrings
+using LightGraphs
+using FFTW
 
 
-M, N=4, 4
-T = Float16
-J= T(1)
+# compute Loschmidt Echo and Work distribution for the BH model with unit filling
 
-function energy_levels(x)
-    U=T(x)
-    graph=chain(M, J, U,:PBC)
-    Ham=BoseHubbard(N,M,J,U,graph).H
-    e=eigen(Array(Ham)).values
-    e
+
+function LE_1D(t::Float64)
+
+    T = eltype(t)
+    J, U = T(1), T(10)
+    
+    #1D parameters
+    M,N=4,4
+    graph = chain(M, J, U, :PBC)
+
+    hamiltonian=BoseHubbard(N, M, J, U, graph) #BH hamiltonian
+    state_i=State([one(T)], [fill(1, M)]) #initial state
+    state_f=expv(-1im*t,hamiltonian,state_i) #final state
+    ket=dense(state_i,NBasis(N,M))
+
+    le=conj(dot(ket,state_f)) #complex conjugate of O(t)
+    le
+end
+
+function LE_2D(t::Float64)
+
+    T = eltype(t)
+    J, U = T(1), T(10)
+
+    #2D parameters
+    dim=(1,2)
+    graph= hexagonal_graph(dim, J, U, :OBC)
+    M = nv(graph)
+    N = Int(M / 1)
+
+    hamiltonian=BoseHubbard(N, M, J, U, graph) #BH hamiltonian
+    state_i=State([one(T)], [fill(1, M)]) #initial state
+    state_f=expv(-1im*t,hamiltonian,state_i) #final state
+    ket=dense(state_i,NBasis(N,M))
+
+    le=abs(dot(ket,state_f))^2 #loschmidt echo
+    le
+end
+
+function work_dis(t::Float64)
+
+    T = eltype(t)
+    J, U = T(1), T(10)
+    
+    #1D parameters
+    M,N=4,4
+    graph = chain(M, J, U, :PBC)
+
+    hamiltonian=BoseHubbard(N, M, J, U, graph) #BH hamiltonian
+    state_i=State([one(T)], [fill(1, M)]) #initial state
+    state_f=expv(-1im*t,hamiltonian,state_i) #final state
+    ket=dense(state_i,NBasis(N,M))
+
+    o=conj(dot(ket,state_f)) #complex conjugate of O(t)
+    o
 end
 
 
-function LE(time)
-    Ui=T(0)
-    Uf=T(1)
-    t=T(time)
-    graphi=chain(M, J, Ui,:PBC)
-    graphf=chain(M, J, Uf,:PBC)
-    Hi=BoseHubbard(N,M,J,Ui,graphi).H
-    Hf=BoseHubbard(N,M,J,Uf,graphf).H
-    ei,vi=eigen(Array(Hi))
-    ef,vf=eigen(Array(Hf))
-    #f=dot(vi,expv(t,Hf,vi))
-    f=sum((abs(dot(vf[k],vi[1]))^2)*exp(-1im*ef[k]*t) for k ∈ 1:length(ef))
-    abs(f)^2
-end
+interval = range(0, 1000, length=1000)
+y = work_dis.(interval)
+F = ifft(y)
+freqs = fftfreq(length(interval))
 
-interval = range(0,20,length=200)
-y = LE.(interval)
-plot(interval, y)
-
+p=plot(freqs,abs.(F),label=L"U=10")
+xlabel!(L"W")
+ylabel!(L"\mathcal{P}(W)")
+#savefig(p, "./examples/work_pdf_1D.pdf")
+p
