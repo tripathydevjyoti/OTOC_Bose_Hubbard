9746ceccc09a30da15a0d6521d34498ef3c39c46
tripathydevjyoti <devjyoti.tripathy@students.iiserpune.ac.in>
2023-04-04 03:34:08 +0530

    bath corr functions

--- src_compare/OTOC.jl
+++ src_compare/OTOC.jl
@@ -1,5 +1,7 @@
 export
-    OTOC
+    OTOC,
+    bath,
+    bath2
 
 """
 $(TYPEDSIGNATURES)
@@ -49,4 +51,43 @@ function OTOC(
 
     # 3. compute OTOC: <y|x>
     dot(ai_V_aj_U_ket, V_aj_U_ai_ket)
+end
+
+function bath(
+    time1::T, time2::T, H::Vector{BoseHubbard{S}}, i::Int, j::Int, state::State; kwargs=()
+) where {S, T <: Real}
+    τ = -1im * time1
+    s = -1im * time2
+
+    
+    evol_ket = expv((τ-s), H[2],state)
+    half_ket = expv(s, H[3], destroy(State(evol_ket,H[2].basis), i))
+    
+    evol_bra = expv(τ, H[2],state)
+    half_bra = dense(destroy(State(evol_bra,H[2].basis),j), H[3].basis)    
+
+    
+    
+    
+    dot(half_bra,half_ket)
+end
+
+function bath2(
+    time1::T, time2::T, H::Vector{BoseHubbard{S}}, i::Int, j::Int, state::State; kwargs=()
+) where {S, T <: Real}
+    τ = -1im * time1
+    s = -1im * time2
+
+    evol_ket = expv((τ-s),H[2],state)
+    adag_i_ket= create(State(evol_ket,H[2].basis),i)
+                
+    state_temp = expv(s,H[1],adag_i_ket)
+
+    U_ai_ket = dense(State(expv(-τ, H[2], destroy(State(state_temp,H[1].basis), j)),H[2].basis), H[2].basis)
+   
+   
+    
+    
+    
+    dot(dense(state,H[2].basis), U_ai_ket)
 end
--- src_compare/lattice.jl
+++ src_compare/lattice.jl
@@ -48,19 +48,27 @@ function chain(M::Int, J::T, U::T, bndr::Symbol) where T <: Real
     lattice(T, inst)
 end
 
+
+
 """
 $(TYPEDSIGNATURES)
 """
+
+
+
 function hexagonal_graph(dim::Dims, J::T, U::T, bndr::Symbol) where T <: Real
     @assert bndr ∈ (:OBC, :PBC)
 
     nx = pyimport("networkx")
-    hg = nx.generators.lattice.hexagonal_lattice_graph(
-        dim..., periodic = bndr == :PBC ? true : false
-    )
+    hg  = nx.Graph()
+    hg.add_nodes_from([1,2,3,4,5,6,7,8])
+    hg.add_edges_from([(1,2),(2,3),(4,5),(5,6),(6,7),(7,8)])
+
     map = Dict(v => i for (i, v) ∈ enumerate(hg.nodes))
     inst = Dict((map[v], map[w]) => J for (v, w) ∈ hg.edges)
     push!(inst, ((map[v], map[v]) => U for v ∈ hg.nodes)...)
 
     lattice(T, inst)
 end
+
+
