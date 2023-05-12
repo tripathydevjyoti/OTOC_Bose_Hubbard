a11ec2eb26c96ca170d56143e1d50ddb3b9bf70f
tripathydevjyoti <devjyoti.tripathy@students.iiserpune.ac.in>
2023-05-12 15:09:01 +0530

    Update df.jl

--- benchmark/df.jl
+++ benchmark/df.jl
@@ -21,18 +21,24 @@ function rhs!(du, u, p, t)
     ρs = reshape(u, (9, 9)) # Density matrix ρs is a 2x2 matrix
     A = p[1] # System operator A
     Adag = p[2]
-    Γ = p[3] # Rate matrix Γ
+    decay_rate_func1 = p[3] # Rate matrix Γ
+    decay_rate-func2 = p[4]
 
     # Compute the time derivative of the density matrix
     du_mat = zeros(9, 9)
     for α in 1:2
         for β in 1:2
             integrand(tau) = (
-                Γ[α, β](t-tau) * (
+                decay_rate_func1(α,β,t,tau) * (
                     A[β](t-tau) * ρs * Adag[α](t) -
                     Adag[α](t) * A[β](t-tau) * ρs
                 )
-            )
+                           + (
+                 decay_rate_func2(α,β,t,tau) * (
+                    Adag[β](t-tau) * ρs * A[α](t) -
+                    A[α](t) * Adag[β](t-tau) * ρs
+                            )
+                ))
             integral, _ = quadgk(integrand, 0, 10)
             du_mat += integral
         end
@@ -40,8 +46,11 @@ function rhs!(du, u, p, t)
     du .= du_mat[:]
 end
 
+   
+
 # Define the initial conditions and time span
-u0 = [1.0 + 0im, 0.0 + 0im, 0.0 + 0im, 0.0 + 0im,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # Initial density matrix ρs is |0><0|
+u0 = [0.0 + 0im, 0.0 + 0im, 0.0 + 0im, 0.0 + 0im,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # Initial density matrix ρs is |0><0|
+u0[19] = 1.0+0im
 tspan = (0.0, 5.0)
 
 # Define the system operator A and rate matrix Γ
@@ -60,45 +69,45 @@ H = BoseHubbard.(NplusBasis.([N-1, N, N+1], M), Ref(graph))
 
 
 
-a1dag = [ 0 0 sqrt(2) 0 0 0 0 0 0;
-       0 0 0 1 0 0 0 0 0;
-       0 0 0 0 0 sqrt(3) 0 0 0;
-       0 0 0 0 0 0 sqrt(2) 0 0;
-       0 0 0 0 0 0 0 1 0;
-       0 0 0 0 0 0 0 0 0;
-       0 0 0 0 0 0 0 0 0;
-       0 0 0 0 0 0 0 0 0;
-       0 0 0 0 0 0 0 0 0]
-
-a2dag = [ 0 0 0 1 0 0 0 0 0;
-          0 0 0 0 sqrt(2) 0 0 0 0;
-          0 0 0 0 0 0 1 0 0;
-          0 0 0 0 0 0 0 sqrt(2) 0;
-          0 0 0 0 0 0 0 0 sqrt(3);
-          0 0 0 0 0 0 0 0 0;
-          0 0 0 0 0 0 0 0 0;
-          0 0 0 0 0 0 0 0 0;
-          0 0 0 0 0 0 0 0 0]
-
-a1 = [ 0 0 0 0 0 0 0 0 0;
+a1dag = [ 0 0 0 0 0 0 0 0 0;
        0 0 0 0 0 0 0 0 0;
        sqrt(2) 0 0 0 0 0 0 0 0;
-       0 1 0 0 0 0 0 0 0;
        0 0 0 0 0 0 0 0 0;
-       0 0 sqrt(3) 0 0 0 0 0 0;
-       0 0 0 sqrt(2) 0 0 0 0 0;
-       0 0 0 0 1 0 0 0 0;
+       0 sqrt(3) 0 0 0 0 0 0 0;
+       0 0 0 0 sqrt(2) 0 0 0 0;
+       0 0 0 0 0 0 0 0 0;
+       0 0 0 0 0 0 0 0 0;
+       0 0 1 0 0 0 0 0 0]
+
+a2dag = [ 0 0 0 0 0 0 0 0 0;
+          0 0 0 0 0 0 0 0 0;
+          0 0 0 0 0 0 sqrt(2) 0 0;
+          0 0 0 0 0 0 0 sqrt(3) 0;
+          1 0 0 0 0 0 0 0 0;
+          0 0 1 0 0 0 0 0 0;
+          0 0 0 0 0 0 0 0 0;
+          0 0 0 0 0 0 0 0 0;
+          0 0 0 sqrt(2) 0 0 0 0 0]
+
+a1 = [ 0 0 sqrt(2) 0 0 0 0 0 0;
+       0 0 0 0 sqrt(3) 0 0 0 0;
+       0 0 0 0 0 0 0 0 1;
+       0 0 0 0 0 0 0 0 0;
+       0 0 0 0 0 sqrt(2) 0 0 0;
+       0 0 0 0 0 0 0 0 0;
+       0 0 0 1 0 0 0 0 0;
+       0 0 0 0 0 0 0 0 0;
        0 0 0 0 0 0 0 0 0] 
 
-a2 = [ 0 0 0 0 0 0 0 0 0;
+a2 = [ 0 0 0 0 1 0 0 0 0;
+       0 0 0 0 0 0 0 0 0;
+       0 0 0 0 0 1 0 0 0;
+       0 0 0 0 0 0 0 0 sqrt(2);
        0 0 0 0 0 0 0 0 0;
        0 0 0 0 0 0 0 0 0;
-       1 0 0 0 0 0 0 0 0
-       0 sqrt(2) 0 0 0 0 0 0 0;
-       0 0 0 0 0 0 0 0 0;
-       0 0 1 0 0 0 0 0 0;
-       0 0 0 sqrt(2) 0 0 0 0 0;
-       0 0 0 0 sqrt(3) 0 0 0 0]       
+       0 0 sqrt(2) 0 0 0 0 0 0;
+       0 0 0 sqrt(3) 0 0 0 0 0;
+       0 0 0 0 0 0 0 0 0]       
        
 
 A1(t) = exp(1im*t*Matrix(H[2].H)) * a1 * exp(-1im*t*Matrix(H[2].H))
@@ -106,7 +115,8 @@ A2(t) = exp(1im*t*Matrix(H[2].H)) * a2 * exp(-1im*t*Matrix(H[2].H))
 A1dag(t) = exp(1im*t*Matrix(H[2].H)) * a1dag * exp(-1im*t*Matrix(H[2].H))
 A2dag(t) = exp(1im*t*Matrix(H[2].H)) * a2dag * exp(-1im*t*Matrix(H[2].H))
 
-
+H[2].basis
+Matrix(H[2].H)
 
 
 
@@ -114,10 +124,94 @@ A2dag(t) = exp(1im*t*Matrix(H[2].H)) * a2dag * exp(-1im*t*Matrix(H[2].H))
 #A2(t) = [0 exp(im*t); 0 0] # A_2(t) operator
 A = [A1, A2] # Vector of system operators
 Adag = [A1dag, A2dag]
-Γ = [t -> (2*sin(t)+1)  t -> (2*t+1); t -> (2*t+1) t -> (2*t+1)] # Rate matrix
+
+
+function bath_corr1( time1::Real ,time2::Real, site1::Int, site2::Int)
+    T = eltype(time1)
+
+    J, U = T(4), T(16)
+    graph = hexagonal_graph( J::T, U::T, :OBC)
+    
+    M = nv(graph)
+    N = Int(M / 1)
+    H = BoseHubbard.(NBasis.([N+1, N, N-1,N-2], M), Ref(graph))
+    
+    state = State([one(T)], [fill(1, M)])
+
+"""
+    if site1 == 1
+        near_neib_site1 = [1,4]
+    else
+        near_neib_site1 = [3,4,8]    
+    end
+    
+    if site2 == 1
+        near_neib_site2 = [1,4]
+    else
+        near_neib_site2 = [3,4,8]    
+    end
+    
+    sum = 0
+    for i in near_neib_site1
+        for j in near_neib_site2
+            sum = sum + bath(time1, time2, H, i, j, state)
+        end    
+    end
+
+    return sum
+"""    
+    
+    return bath(time1,time2,H,site1,site2,state)
+end  
+
+function bath_corr2( time1::Real, time2::Real, site1::Int, site2::Int)
+    T = eltype(time1)
+    J, U = T(4), T(16)
+    graph = hexagonal_graph( J::T, U::T, :OBC)
+    
+    M = nv(graph)
+    N = Int(M / 1)
+    H = BoseHubbard.(NBasis.([N+1, N, N-1,N-2], M), Ref(graph))
+    
+    state = State([one(T)], [fill(1, M)])
+   """ 
+    if site1 == 1
+        near_neib_site1 = [1,4]
+    else
+        near_neib_site1 = [3,4,8]    
+    end
+    
+    if site2 == 1
+        near_neib_site2 = [1,4]
+    else
+        near_neib_site2 = [3,4,8]    
+    end
+    
+    sum = 0
+    for i in near_neib_site1
+        for j in near_neib_site2
+            sum = sum + bath2(time1, time2, H, i, j, state)
+        end    
+    end
+
+    return sum
+  """
+    return bath2(time1,time2,H,site1,site2,state)  
+end
+
+
+
+function decay_rate_func1(α,β,t,tau)
+    return bath_corr1(t,tau,β,α)
+end
+
+function decay_rate_func2(α,β,t,tau)
+    return bath_corr2(t,tau,β,α)
+end    
+
 
 # Define the parameter vector
-p = [A, Adag, Γ]
+p = [A, Adag, decay_rate_func1,decay_rate_func2]
 
 # Define the ODE problem
 prob = ODEProblem(rhs!, u0, tspan, p)
@@ -127,9 +221,11 @@ prob = ODEProblem(rhs!, u0, tspan, p)
 sol = solve(prob)
 
 t = sol.t
-ρ12 = [sol[i][2] for i in 1:length(t)]
+ρ12 = [sol[i][6] for i in 1:length(t)]
 using Plots
-plot(t,imag(ρ12))
+scatter(t,abs.(ρ12))
+plot(t,abs.(ρ12))
+
 
 
 
