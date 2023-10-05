4b7b635fb7fccd04d397592bae5f18dae291179e
tripathydevjyoti <devjyoti.tripathy@students.iiserpune.ac.in>
2023-10-05 15:28:54 -0400

    update complexity coeff

--- benchmark/complexity.jl
+++ benchmark/complexity.jl
@@ -3,11 +3,11 @@ using .OTOC_Bose_Hubbard
 using Test
 using LightGraphs
 using LabelledGraphs
-
+using LinearAlgebra
 using Plots
 using PyCall
 
-dim=(1,1)
+
 time1 =0.1
 T = eltype(time1)
 J, U = T(4), T(16)
@@ -18,6 +18,64 @@ N = Int(M / 1)
 H = BoseHubbard.(NBasis.([N, N-1, N-2], M), Ref(graph))
 H[1].H
 
-i=1
+site =3
 state = State([one(T)], [fill(1, M)])
-length(H[1].basis.eig_vecs)
+
+
+dim_ham = length(H[1].basis.eig_vecs)
+op_zero = zeros((dim_ham,dim_ham))
+
+
+for i in 1:dim_ham
+    op_zero[i,i] = float(H[1].basis.eig_vecs[i][site])
+end    
+
+
+function super_op(H,op)
+    return H*op - op*H
+
+end  
+
+op_one = super_op(H[1].H,op_zero)
+
+function norm(op)
+    sqrt(tr(adjoint(op)*op))
+end 
+
+
+
+
+
+b1 = norm(op_one)
+op_one = op_one/b1
+
+op_basis = [op_one]
+coeff_basis = [b1]
+
+n_iter = 10
+
+for i in 2:n_iter
+
+    if i == 2
+        new_op = super_op(H[1].H,op_basis[1]) - coeff_basis[1]*op_zero
+        new_coeff = norm(new_op)
+        push!(op_basis, new_op/new_coeff)
+        push!(coeff_basis, new_coeff)
+
+    else
+
+        new_op = super_op(H[1].H,op_basis[i-1]) - coeff_basis[i-1]*op_basis[i-2]
+        new_coeff = norm(new_op)
+        push!(op_basis, new_op/new_coeff)
+        push!(coeff_basis, new_coeff)
+    end    
+
+end    
+
+coeff_basis
+
+x_axis = [1,2,3,4,5,6,7,8,9,10]
+
+plot(x_axis, coeff_basis)
+
+    
