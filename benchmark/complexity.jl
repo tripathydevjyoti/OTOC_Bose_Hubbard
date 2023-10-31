f69a12f188ae3524e3cf5e58b1cc29d54f9321ae
tripathydevjyoti <devjyoti.tripathy@students.iiserpune.ac.in>
2023-10-31 14:53:56 -0400

    Update complexity.jl

--- benchmark/complexity.jl
+++ benchmark/complexity.jl
@@ -10,17 +10,18 @@ using PyCall
 
 time1 =0.1
 T = eltype(time1)
-J, U = T(4), T(10)
+J, U = T(4), T(16)
 dim =(1,1)
 graph = hexagonal_graph(dim, J::T, U::T, :OBC)
     #graph = hex_graph(T(4),T(16))
 M = nv(graph)
 N = Int(M / 1)
 H = BoseHubbard.(NBasis.([N, N-1, N-2], M), Ref(graph))
-H[1].H
+#H = BoseHubbard(6,6 ,J, U, :OBC)
+bh_hamil = H[1].H
 
 site =3
-state = State([one(T)], [fill(1, M)])
+
 
 
 dim_ham = length(H[1].basis.eig_vecs)
@@ -31,6 +32,7 @@ for i in 1:dim_ham
     op_zero[i,i] = float(H[1].basis.eig_vecs[i][site])
 end    
 
+op_zero
 
 function super_op(H,op)
     return H*op - op*H
@@ -38,38 +40,40 @@ function super_op(H,op)
 end  
 
 
-
-function norm(op)
-    sqrt(tr(adjoint(op)*op))
-end 
-
 function trace_norm(op1,op2)
     tr(adjoint(op1)*op2)
 end
 
+function norm(op)
+    sqrt(trace_norm(op,op))
+end 
+
+
+
 
 op_zero = op_zero/norm(op_zero)
 
-op_one = super_op(H[1].H,op_zero)
+op_one = super_op(Matrix(bh_hamil),op_zero)
 b1 = norm(op_one)
 op_one = op_one/b1
 
 op_basis = [op_one]
 coeff_basis = [b1]
 
-n_iter = 100
+n_iter = 461
+   
 
 for i in 2:n_iter
 
     if i == 2
-        new_op = super_op(H[1].H,op_basis[1]) - coeff_basis[1]*op_zero
+        new_op = super_op(bh_hamil,op_basis[1]) - coeff_basis[1]*op_zero
         new_coeff = norm(new_op)
         push!(op_basis, new_op/new_coeff)
         push!(coeff_basis, new_coeff)
 
     else
 
-        new_op = super_op(H[1].H,op_basis[i-1]) - coeff_basis[i-1]*op_basis[i-2]
+        new_op = super_op(bh_hamil,op_basis[i-1]) - coeff_basis[i-1]*op_basis[i-2]
         new_coeff = norm(new_op)
         push!(op_basis, new_op/new_coeff)
         push!(coeff_basis, new_coeff)
@@ -82,36 +86,41 @@ op_basis
 #y_axis = [coeff_basis[1:10]]
 
 x_axis = collect(1:n_iter)
-sf_phase = coeff_basis
-plot(x_axis,[coeff_basis,sf_phase], linewidth=3,  label=["U/J =4" "U/J=0.25"])
+coup4= coeff_basis
+coup3 = coeff_basis
+coup2 =coeff_basis
+coup1 = coeff_basis
+couphalf = coeff_basis
+plot(x_axis,[couphalf,coup1,coup2,coup3,coup4,coeff_basis], linewidth=3,  label=["U/J =0.5" "U/J =1" "U/J =2" "U/J =3" "U/J =4"])
+plot(x_axis,coeff_basis)
 xlabel!("n")
 ylabel!("Lanczos Coefficients")
 savefig("/Users/dev/Desktop/coeff_BH.pdf")
 
 
-Matrix(H[1].H)
+Matrix(bh_hamil)
 function time_evol_op(time, op, hamil)
     exp(1im*time*hamil)*op*exp(-1im*time*hamil)
 end
 
-times = range(0.01, stop=5, length=20)
+times = range(0.0, stop=15, length=20)
 time_axis = collect(times)
 complexity = []
 
 for i in 1:length(time_axis)
-    op_t = time_evol_op(time_axis[i], op_zero, Matrix(H[1].H))
+    op_t = time_evol_op(time_axis[i], op_zero,Matrix(bh_hamil))
     sum = 0
-    phi_zero = trace_norm(op_t,op_zero)
+    phi_zero = trace_norm(op_zero,op_t)
     sum =sum + 0*abs(phi_zero)*abs(phi_zero)
     
     for j in 1:n_iter
-        phi_j = trace_norm(op_t,op_basis[j])
+        phi_j = trace_norm(op_basis[j],op_t)
         sum = sum + j*abs(phi_j)*abs(phi_j)
     end
     push!(complexity,sum)
 end
-sf_complexity = complexity
-plot(time_axis,[mott_complexity,complexity],linewidth = 2.5,label =["U/J=4" "U/J=0.25"])
+
+plot(time_axis,[complexity],linewidth = 2.5,label =["U/J=4"])
 vline!([0.25], label="OTOC Decay", line=(color="black", linestyle=:dash))
 xlabel!("t")
 ylabel!("Krylov Complexity")
@@ -131,6 +140,19 @@ sorted_eigenvalues = sort(eigenvalues)
 
 # Calculate level spacings
 level_spacings = diff(sorted_eigenvalues)
+num = length(level_spacings)
+r_alpha = []
+for i in 1:num
+    if i!= num
+        ra = level_spacings[i+1]/level_spacings[i]
+        push!(r_alpha,min(ra,(1/ra)))
+    end
+end        
+r_alpha       
+histogram(r_alpha, bins=100 )
+mean(r_alpha)
+
+sort(level_spacings)
 using Statistics
 avg = mean(level_spacings)
 # Plot the level spacing distribution
